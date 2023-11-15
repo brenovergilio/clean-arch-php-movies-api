cad144ac0427a216ade5cbb08db65332a467fc6d
Breno Vergilio <bjvergilio47@gmail.com>
2023-11-15 15:56:45 -0400

    feat: use Email class inside user class

--- app/Core/Domain/Entitites/User/User.php
+++ app/Core/Domain/Entitites/User/User.php
@@ -8,10 +8,12 @@ class User {
     private string|int $id,
     private string $name,
     string $cpf,
+    string $email,
     private string $photo,
     private bool $emailConfirmed
   ) {
     $this->cpf = new CPF($cpf);
+    $this->email = new Email($email);
   }
 
   public function isEmailConfirmed(): bool {
