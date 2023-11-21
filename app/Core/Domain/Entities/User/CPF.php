305a6ae401c40e75b2c2e7422d6f5cf47bc01e2d
Breno Vergilio <bjvergilio47@gmail.com>
2023-11-21 17:30:20 -0400

    test: add CPF unit tests

--- app/Core/Domain/Entities/User/CPF.php
+++ app/Core/Domain/Entities/User/CPF.php
@@ -4,6 +4,7 @@ namespace App\Core\Domain\Entities\User;
 use App\Core\Domain\Exceptions\InvalidFieldException;
 
 class CPF {
+
   public function __construct(
     private ?string $value
   ) {
@@ -16,35 +17,15 @@ class CPF {
     return $this->value;
   }
 
-  private function keepJustNumbers(string $value): string {
+  private function keepJustNumbers(?string $value): ?string {
+    if(!$value) return null;
     return preg_replace('/[^0-9]/', '', $value);
   }
 
   private function validateCPF() {
     $fieldName = "CPF";
 
-    if(!isset($this->value)) return;
-
-    if(strlen($this->value !== 11)) throw new InvalidFieldException($fieldName);
-
-    if(preg_match('/^(\d)\1+$/', $this->value)) throw new InvalidFieldException($fieldName);
-
-    for ($i = 9, $j = 10, $sum = 0; $i > 0; $i--, $j--) {
-      $sum += $this->value[$i - 1] * $j;
-    }
-
-    $rest = $sum % 11;
-    $firstVerifyingDigit = ($rest < 2) ? 0 : 11 - $rest;
-
-    if ($this->value[9] != $firstVerifyingDigit) throw new InvalidFieldException($fieldName);
-
-    for ($i = 10, $j = 11, $sum = 0; $i > 0; $i--, $j--) {
-        $sum += $this->value[$i - 1] * $j;
-    }
-
-    $rest = $sum % 11;
-    $secondVerifyingDigit = ($rest < 2) ? 0 : 11 - $rest;
-
-    if ($this->value[10] != $secondVerifyingDigit) throw new InvalidFieldException($fieldName);
+    if(!$this->value) return;
+    if(strlen($this->value) !== 11) throw new InvalidFieldException($fieldName);
   }
 }
--- tests/Unit/Core/Domain/Entities/User/CPFTest.php
+++ tests/Unit/Core/Domain/Entities/User/CPFTest.php
@@ -6,3 +6,14 @@ it("should return a CPF with numbers only", function () {
   $cpf = new CPF("146.290.370-39");
   expect($cpf->getValue())->toBe("14629037039");
 });
+
+it("should return null because parameter was null", function () {
+  $cpf = new CPF(null);
+  expect($cpf->getValue())->toBe(null);
+});
+
+it("should throw an InvalidFieldException because parameter does not has 11 digits", function () {
+  expect(function() {
+    new CPF("146.290.370-392");
+  })->toThrow("Field CPF is invalid");
+});
