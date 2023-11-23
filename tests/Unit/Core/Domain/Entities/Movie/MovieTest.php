77b68e084d0e7bb827ef9a3b28205f35fd115888
Breno Vergilio <bjvergilio47@gmail.com>
2023-11-23 00:12:54 -0400

    test: add movie unit tests

---
+++ tests/Unit/Core/Domain/Entities/Movie/MovieTest.php
@@ -0,0 +1,64 @@
+<?php
+use App\Core\Domain\Entities\Movie\Movie;
+use App\Core\Domain\Entities\Movie\MovieGenre;
+use DateTime;
+
+beforeEach(function() {
+  $this->today = new DateTime();
+  $this->movie = new Movie('id', 'title', 'synopsis', 'directorName', MovieGenre::ACTION, "pathToCover", true, $this->today, $this->today);
+});
+
+it("should return the right values when using getters", function () {
+  expect($this->movie->id())->toBe("id");
+  expect($this->movie->title())->toBe("title");
+  expect($this->movie->synopsis())->toBe("synopsis");
+  expect($this->movie->directorName())->toBe("directorName");
+  expect($this->movie->genre())->toBe(MovieGenre::ACTION);
+  expect($this->movie->cover())->toBe("pathToCover");
+  expect($this->movie->isPublic())->toBe(true);
+  expect($this->movie->releaseDate())->toBe($this->today);
+  expect($this->movie->addedAt())->toBe($this->today);
+});
+
+it("should change movie's title", function() {
+  expect($this->movie->title())->toBe("title");
+  $this->movie->changeTitle("new title");
+  expect($this->movie->title())->toBe("new title");
+});
+
+it("should change movie's synopsis", function() {
+  expect($this->movie->synopsis())->toBe("synopsis");
+  $this->movie->changeSynopsis("new synopsis");
+  expect($this->movie->synopsis())->toBe("new synopsis");
+});
+
+it("should change movie's director name", function() {
+  expect($this->movie->directorName())->toBe("directorName");
+  $this->movie->changeDirectorName("new directorName");
+  expect($this->movie->directorName())->toBe("new directorName");
+});
+
+it("should change movie's genre", function() {
+  expect($this->movie->genre())->toBe(MovieGenre::ACTION);
+  $this->movie->changeGenre(MovieGenre::COMEDY);
+  expect($this->movie->genre())->toBe(MovieGenre::COMEDY);
+});
+
+it("should change movie's cover path", function() {
+  expect($this->movie->cover())->toBe("pathToCover");
+  $this->movie->changeCover("pathToNewCover");
+  expect($this->movie->cover())->toBe("pathToNewCover");
+});
+
+it("should change movie's visibility", function() {
+  expect($this->movie->isPublic())->toBe(true);
+  $this->movie->changeVisibility(false);
+  expect($this->movie->isPublic())->toBe(false);
+});
+
+it("should change movie's release date", function() {
+  $yesterday = $this->today->modify('-1 day');
+  expect($this->movie->releaseDate())->toBe($this->today);
+  $this->movie->changeReleaseDate($yesterday);
+  expect($this->movie->releaseDate())->toBe($yesterday);
+});
--- tests/Unit/Core/Domain/Entities/User/UserTest.php
+++ tests/Unit/Core/Domain/Entities/User/UserTest.php
@@ -23,39 +23,39 @@ it("should return true when calling isEmailConfirmed()", function() {
   expect($this->user->isEmailConfirmed())->toBeTrue();
 });
 
-it("should return confirm user email", function() {
+it("should return confirm user's email", function() {
   $user = new User("id", "name", "146.290.370-39", "valid@mail.com", "hashedPassword", Role::ADMIN, "pathToPhoto", false);
   expect($user->isEmailConfirmed())->toBeFalse();
   $user->confirmEmail();
   expect($user->isEmailConfirmed())->toBeTrue();
 });
 
-it("should change user password", function() {
+it("should change user's password", function() {
   expect($this->user->password())->toBe("hashedPassword");
   $this->user->changePassword("newHashedPassword");
   expect($this->user->password())->toBe("newHashedPassword");
 });
 
-it("should change user photo path", function() {
+it("should change user's photo path", function() {
   expect($this->user->photo())->toBe("pathToPhoto");
   $this->user->changePhoto("pathToNewPhoto");
   expect($this->user->photo())->toBe("pathToNewPhoto");
 });
 
-it("should change user name", function() {
+it("should change user's name", function() {
   expect($this->user->name())->toBe("Name");
   $this->user->changeName("Another Name");
   expect($this->user->name())->toBe("Another Name");
 });
 
-it("should change user email and set emailConfirmed property to false after that", function() {
+it("should change user's email and set emailConfirmed property to false after that", function() {
   expect($this->user->email())->toBe("valid@mail.com");
   $this->user->changeEmail("anotherValid@mail.com");
   expect($this->user->email())->toBe("anotherValid@mail.com");
   expect($this->user->isEmailConfirmed())->toBeFalse();
 });
 
-it("should change user CPF", function() {
+it("should change user's CPF", function() {
   expect($this->user->cpf())->toBe("14629037039");
   $this->user->changeCPF("904.051.380-59");
   expect($this->user->cpf())->toBe("90405138059");
