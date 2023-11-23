44f2329e5c6e95dfe0e1dbcd865734ccbb1336e9
Breno Vergilio <bjvergilio47@gmail.com>
2023-11-22 23:06:40 -0400

    test: add login unit tests

---
+++ tests/Unit/Core/Application/UseCases/Auth/Login/LoginUseCaseTest.php
@@ -0,0 +1,70 @@
+<?php
+use App\Core\Application\Interfaces\HashComparer;
+use App\Core\Application\Interfaces\TokenGenerator;
+use App\Core\Application\UseCases\Auth\Login\DTO\LoginInputDTO;
+use App\Core\Application\UseCases\Auth\Login\LoginUseCase;
+use App\Core\Domain\Entities\User\UserRepository;
+use App\Core\Domain\Entities\User\User;
+
+beforeEach(function() {
+  $this->userRepositoryMock = Mockery::mock(UserRepository::class);
+  $this->hashComparerMock = Mockery::mock(HashComparer::class);
+  $this->tokenGeneratorMock = Mockery::mock(TokenGenerator::class);
+
+  $this->user = new User("id", "name", "146.290.370-39", "valid@mail.com", "password", \App\Core\Domain\Entities\User\Role::CLIENT, 'photo', true);
+  $this->inputDto = new LoginInputDTO(
+    $this->user->email(),
+    $this->user->password()
+  );
+
+  $this->sut = new LoginUseCase(
+    $this->userRepositoryMock,
+    $this->hashComparerMock,
+    $this->tokenGeneratorMock
+  );
+});
+
+it('should throw an InvalidCredentialsException because user was not found', function() {
+  $this->userRepositoryMock->shouldReceive('findByEmail')->andReturn(null);
+
+  expect(function() {
+    $this->sut->execute($this->inputDto);
+  })->toThrow("Invalid Credentials");
+});
+
+it("should throw an InvalidCredentialsException because the password provided does not match user's password", function() {
+  $this->userRepositoryMock->shouldReceive('findByEmail')->andReturn($this->user);
+  $this->hashComparerMock->shouldReceive('compare')->andReturn(false);
+
+  expect(function() {
+    $this->sut->execute($this->inputDto);
+  })->toThrow("Invalid Credentials");
+});
+
+it("should call generate() with right values", function() {
+  $this->userRepositoryMock->shouldReceive('findByEmail')->andReturn($this->user);
+  $this->hashComparerMock->shouldReceive('compare')->andReturn(true);
+  
+  $this->tokenGeneratorMock->shouldReceive('generate')->with(Mockery::on(function ($argument) {
+    return $argument->id() === $this->user->id() &&
+           $argument->name() === $this->user->name() &&
+           $argument->cpf() === $this->user->cpf() &&
+           $argument->email() === $this->user->email() &&
+           $argument->password() === $this->user->password() &&
+           $argument->photo() === $this->user->photo();
+  }), ['id'])->once();
+
+  $this->sut->execute($this->inputDto);
+});
+
+it("should return expected data in DTO", function() {
+  $this->userRepositoryMock->shouldReceive('findByEmail')->andReturn($this->user);
+  $this->hashComparerMock->shouldReceive('compare')->andReturn(true);
+  $this->tokenGeneratorMock->shouldReceive('generate')->andReturn("token");
+
+  $result = $this->sut->execute($this->inputDto);
+  
+  expect($result->id)->toBe($this->user->id());
+  expect($result->token)->toBe("token");
+  expect($result->isAdmin)->toBe($this->user->isAdmin());
+});
