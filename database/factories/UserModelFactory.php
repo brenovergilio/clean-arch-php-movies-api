9348d1ae9bff2070061e5bd058fcfaf00f023e82
Breno Vergilio <bjvergilio47@gmail.com>
2023-11-23 20:00:02 -0400

    feat: seed database with users

--- database/factories/UserModelFactory.php
+++ database/factories/UserModelFactory.php
@@ -25,7 +25,7 @@ class UserModelFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'cpf' => (string) fake()->unique()->randomNumber(11),
+            'cpf' => null,
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,16 +2,37 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Infra\Handlers\BcryptHandler;
+use App\Models\UserModel;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
+    public function __construct(
+        private BcryptHandler $bcryptHandler
+    ) {}
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        
+        UserModel::factory()->createOne([
+            'name' => 'Administrator',
+            'email' => 'admin@mail.com',
+            'password' => $this->bcryptHandler->generate('password')
+        ]);
+
+        UserModel::factory()->client()->createOne([
+            'name' => 'Verified User',
+            'email' => 'verified@mail.com',
+            'password' => $this->bcryptHandler->generate('password')
+        ]);
+
+        UserModel::factory()->client()->unverified()->createOne([
+            'name' => 'Unverified User',
+            'email' => 'unverified@mail.com',
+            'password' => $this->bcryptHandler->generate('password')
+        ]);
     }
 }
