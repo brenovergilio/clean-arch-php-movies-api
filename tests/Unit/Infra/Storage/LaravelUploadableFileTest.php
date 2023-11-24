407c983c621fc574750e2e7f2b653328dde2b6e8
Breno Vergilio <bjvergilio47@gmail.com>
2023-11-24 00:16:05 -0400

    test: add LaravelUploadableFile unit tests

---
+++ tests/Unit/Infra/Storage/LaravelUploadableFileTest.php
@@ -0,0 +1,11 @@
+<?php
+use App\Infra\Storage\LaravelUploadableFile;
+use Illuminate\Http\UploadedFile;
+
+it("should return path", function() {
+  $uploadableFile = Mockery::mock(UploadedFile::class);
+  $uploadableFile->shouldReceive('store')->andReturn("path");
+  
+  $sut = new LaravelUploadableFile($uploadableFile);
+  expect($sut->upload())->toBe("path");
+});
