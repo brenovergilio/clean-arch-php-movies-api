2f2082c2fbc4731cd42e052f555f35c14ed45b93
Breno Vergilio <bjvergilio47@gmail.com>
2023-11-24 01:21:59 -0400

    feat: add required filed validation

---
+++ app/Presentation/Validations/RequiredFieldValidation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Presentation\Validations;
+use App\Core\Domain\Exceptions\MissingRequiredFieldException;
+use App\Presentation\Interfaces\Validation;
+use Exception;
+
+class RequiredFieldValidation implements Validation {
+  public function __construct(private string $fieldName) {}
+
+  public function validate($input): ?Exception {
+    if(!$input[$this->fieldName]) return new MissingRequiredFieldException($this->fieldName);
+    return null;
+  }
+}
