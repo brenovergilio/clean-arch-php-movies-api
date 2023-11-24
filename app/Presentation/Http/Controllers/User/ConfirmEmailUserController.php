3967e22b1ea8584166de06c5e2dc3d3b2d2c7ec3
Breno Vergilio <bjvergilio47@gmail.com>
2023-11-24 16:36:19 -0400

    test: add confirm email user controller unit tests

---
+++ app/Presentation/Http/Controllers/User/ConfirmEmailUserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Presentation\Http\Controllers\User;
+use App\Core\Application\UseCases\User\ConfirmEmail\ConfirmEmailUseCase;
+use App\Core\Application\UseCases\User\ConfirmEmail\DTO\ConfirmEmailInputDTO;
+use App\Core\Domain\Exceptions\EntityNotFoundException;
+use App\Core\Domain\Exceptions\MissingRequiredFieldException;
+use App\Presentation\Http\HttpStatusCodes;
+use App\Presentation\Http\HttpRequest;
+use App\Presentation\Http\HttpResponse;
+
+class ConfirmEmailUserController {
+  public function __construct(private ConfirmEmailUseCase $useCase) {}
+
+  public function confirmEmail(HttpRequest $request): HttpResponse {
+    try {
+      $validationException = UserControllerValidations::confirmEmailValidations()->validate($request->body);
+
+      if($validationException) throw $validationException;
+
+      $inputDto = new ConfirmEmailInputDTO(
+        $request->body['accessToken']
+      );
+
+      $this->useCase->execute($inputDto);
+      return new HttpResponse(null, HttpStatusCodes::NO_CONTENT);
+    } catch (MissingRequiredFieldException $exception) {
+      return new HttpResponse(["error" => $exception->getMessage()], HttpStatusCodes::BAD_REQUEST);
+    } catch (EntityNotFoundException $exception) {
+      return new HttpResponse(["error" => $exception->getMessage()], HttpStatusCodes::NOT_FOUND);
+    }
+  }
+}
---
+++ tests/Unit/Presentation/Http/Controllers/User/ConfirmEmailUserControllerTest.php
@@ -0,0 +1,50 @@
+<?php
+use App\Core\Application\UseCases\User\ConfirmEmail\ConfirmEmailUseCase;
+use App\Core\Domain\Exceptions\EntityNotFoundException;
+use App\Presentation\Http\Controllers\User\ConfirmEmailUserController;
+use App\Presentation\Http\HttpStatusCodes;
+use App\Presentation\Http\HttpRequest;
+
+beforeEach(function() {
+  $this->confirmEmailUseCaseMock = Mockery::mock(ConfirmEmailUseCase::class);
+  $this->sut = new ConfirmEmailUserController($this->confirmEmailUseCaseMock);
+});
+
+afterEach(function() {
+  Mockery::close();
+});
+
+it('should return 400 status code because required field accessToken is missing', function() {
+  $body = [];
+  $httpRequest = new HttpRequest($body);
+
+  $result = $this->sut->confirmEmail($httpRequest);
+  expect($result->statusCode)->toBe(HttpStatusCodes::BAD_REQUEST);
+  expect($result->body['error'])->toBe("Field accessToken is missing");
+});
+
+it('should return 404 status code because use case thrown EntityNotFoundException', function() {
+  $body = [
+    "accessToken" => "A1B2C3"
+  ];
+  $httpRequest = new HttpRequest($body);
+
+  $this->confirmEmailUseCaseMock->shouldReceive('execute')->andThrow(new EntityNotFoundException("Entity"));
+
+  $result = $this->sut->confirmEmail($httpRequest);
+  expect($result->statusCode)->toBe(HttpStatusCodes::NOT_FOUND);
+  expect($result->body['error'])->toBe("Entity Entity not found");
+});
+
+it('should return 204 status code in case of success', function() {
+  $body = [
+    "accessToken" => "A1B2C3"
+  ];
+
+  $this->confirmEmailUseCaseMock->shouldReceive('execute')->once();
+  $httpRequest = new HttpRequest($body);
+
+  $result = $this->sut->confirmEmail($httpRequest);
+  expect($result->statusCode)->toBe(HttpStatusCodes::NO_CONTENT);
+  expect($result->body)->toBeNull();
+});
