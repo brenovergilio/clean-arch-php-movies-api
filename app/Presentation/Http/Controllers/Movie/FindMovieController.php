ba2a586da9c594802c14faa08d97cb450632ce12
Breno Vergilio <bjvergilio47@gmail.com>
2023-11-24 17:12:19 -0400

    feat: add find movie controller

---
+++ app/Presentation/Http/Controllers/Movie/FindMovieController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Presentation\Http\Controllers\Movie;
+use App\Core\Application\UseCases\Movie\FindMovie\DTO\FindMovieInputDTO;
+use App\Core\Application\UseCases\Movie\FindMovie\FindMovieUseCase;
+use App\Core\Domain\Exceptions\EntityNotFoundException;
+use App\Presentation\Http\HttpStatusCodes;
+use App\Presentation\Http\HttpResponse;
+
+class FindMovieController {
+  public function __construct(private FindMovieUseCase $useCase) {}
+
+  public function update(string|int $id): HttpResponse {
+    try {
+      
+      $inputDto = new FindMovieInputDTO(
+        $id
+      );
+
+      $result = $this->useCase->execute($inputDto);
+      return new HttpResponse(["data" => $result], HttpStatusCodes::OK);
+    } catch (EntityNotFoundException $exception) {
+      return new HttpResponse(["error" => $exception->getMessage()], HttpStatusCodes::NOT_FOUND);
+    }
+  }
+}
