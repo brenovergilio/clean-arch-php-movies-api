7195f295d1e53782869af9f7df5d8b87ce2ed993
Breno Vergilio <bjvergilio47@gmail.com>
2023-11-25 16:52:48 -0400

    feat: add primitive field validation

---
+++ app/Presentation/Validations/PrimitiveTypeValidation.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Presentation\Validations;
+use App\Core\Domain\Exceptions\InvalidFieldException;
+use App\Presentation\Validations\Interfaces\Validation;
+use Exception;
+
+class InstanceOfValidation implements Validation {
+  public function __construct(private string $fieldName, private string $primitiveType) {}
+
+  public function validate($input): ?Exception {
+    $field = $input[$this->fieldName];
+
+    switch ($this->primitiveType) {
+      case 'string':
+          if(!is_string($field)) return new InvalidFieldException($this->fieldName);
+          break;
+      case 'int':
+          if(!is_int($field)) return new InvalidFieldException($this->fieldName);
+          break;
+      case 'float':
+          if(!is_float($field)) return new InvalidFieldException($this->fieldName);
+          break;
+      case 'bool':
+          if(!is_bool($field)) return new InvalidFieldException($this->fieldName);
+          break;
+      default:
+          return null;
+    }
+
+    return null;
+  }
+}
